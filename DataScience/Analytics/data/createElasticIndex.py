25e2f61bf20226409d39cdc93551fce8488193df
Sami Niemi <Sami.Niemi@valtech.co.uk>
2017-01-31 09:23:43 +0000

    Added documentation and cleaned the code.

--- DataScience/Analytics/data/createElasticIndex.py
+++ DataScience/Analytics/data/createElasticIndex.py
@@ -1,29 +1,99 @@
 """
+Create ElasticSearch Hybrid Index
+=================================
 
+A simple script to create an Elastic Search index from an AddressBase Hybrid CSV file.
+The script does not define mappings or any specific configuration. Can therefore be
+used for testing only.
+
+
+Requirements
+------------
+
+:requires: elastisearch (2.4.0)
+:requires: pandas (0.19.1)
+:requires: numpy (1.11.2)
+
+
+Author
+------
+
+:author: Sami Niemi (sami.niemi@valtech.co.uk)
+
+
+Version
+-------
+
+:version: 0.1
+:date: 31-Jan-2017
 """
-import pandas as pd
 import numpy as np
+import pandas as pd
 from elasticsearch import Elasticsearch
 
 
-def load_data(filename='ABtest.csv', path='/Users/saminiemi/Projects/ONS/AddressIndex/data/ADDRESSBASE/'):
-    data = pd.read_csv(path + filename,
-                       dtype={'UPRN': np.int64, 'POSTCODE_LOCATOR': str, 'ORGANISATION_NAME': str,
-                              'DEPARTMENT_NAME': str, 'SUB_BUILDING_NAME': str, 'BUILDING_NAME': str,
-                              'BUILDING_NUMBER': str, 'THROUGHFARE': str, 'DEPENDENT_LOCALITY': str,
-                              'POST_TOWN': str, 'POSTCODE': str, 'PAO_TEXT': str,
-                              'PAO_START_NUMBER': str, 'PAO_START_SUFFIX': str, 'PAO_END_NUMBER': str,
-                              'PAO_END_SUFFIX': str, 'SAO_TEXT': str, 'SAO_START_NUMBER': np.float64,
-                              'SAO_START_SUFFIX': str, 'ORGANISATION': str, 'STREET_DESCRIPTOR': str,
-                              'TOWN_NAME': str, 'LOCALITY': str})
-    print('Found {} addresses from AddressBase...'.format(len(data.index)))
+def load_data(filename='AB_processed.csv', path='/Users/saminiemi/Projects/ONS/AddressIndex/data/ADDRESSBASE/',
+              test=False):
+    """
+    Load AddressBase data to a Pandas DataFrame.
+    As a quick fix, fill nones
+
+    :param filename: name of the CSV file to load AB from
+    :type filename: str
+    :param path: location of the AB file
+    :type path: str
+    :param test: whether or not to populated the test index
+    :type test: bool
+
+    :return: AddressBase
+    :type: pandas.DataFrame
+    """
+    if test:
+        filename = 'ABtest.csv'
+        dtype = {'UPRN': np.int64, 'POSTCODE_LOCATOR': str, 'ORGANISATION_NAME': str,
+                 'DEPARTMENT_NAME': str, 'SUB_BUILDING_NAME': str, 'BUILDING_NAME': str,
+                 'BUILDING_NUMBER': str, 'THROUGHFARE': str, 'DEPENDENT_LOCALITY': str,
+                 'POST_TOWN': str, 'POSTCODE': str, 'PAO_TEXT': str,
+                 'PAO_START_NUMBER': np.float64, 'PAO_START_SUFFIX': str, 'PAO_END_NUMBER': np.float64,
+                 'PAO_END_SUFFIX': str, 'SAO_TEXT': str, 'SAO_START_NUMBER': np.float64,
+                 'SAO_START_SUFFIX': str, 'ORGANISATION': str, 'STREET_DESCRIPTOR': str,
+                 'TOWN_NAME': str, 'LOCALITY': str}
+    else:
+        dtype = {'UPRN': np.int64, 'ORGANISATION_NAME': str,
+                 'DEPARTMENT_NAME': str, 'SUB_BUILDING_NAME': str, 'BUILDING_NAME': str,
+                 'BUILDING_NUMBER': str, 'THROUGHFARE': str,
+                 'POST_TOWN': str, 'POSTCODE': str, 'PAO_TEXT': str,
+                 'PAO_START_NUMBER': np.float64, 'PAO_START_SUFFIX': str,
+                 'SAO_TEXT': str, 'SAO_START_NUMBER': np.float64,
+                 'STREET_DESCRIPTOR': str, 'TOWN_NAME': str, 'LOCALITY': str,
+                 'postcode_in': str, 'postcode_out': str}
+
+    data = pd.read_csv(path + filename, dtype=dtype)
+    print('\nFound {} addresses from AddressBase...'.format(len(data.index)))
+    print(data.info())
 
     data.fillna('', inplace=True)
 
     return data
 
 
-def create_index(data, index_name='addressbase', type_name='hybrid'):
+def create_and_populate_index(data, index_name='addressbase', type_name='hybrid'):
+    """
+    Creates and populates a new index with given data.
+
+    If an index exists then deletes the old one before creating a new one.
+    Converts the given dataframe to lists with dictionaries for bulk population.
+    This is not very efficient as requires iterating over the data.
+
+    :param data: input data to be indexed
+    :type data: pandas.DataFrame
+    :param index_name: name of the index to create
+    :type index_name: str
+    :param type_name: name of the type
+    :type type_name: str
+
+    :return: None
+    """
     es = Elasticsearch(hosts=[{'host': 'localhost', 'port': 9200}])
 
     if es.indices.exists(index_name):
@@ -32,7 +102,7 @@ def create_index(data, index_name='addressbase', type_name='hybrid'):
         print('Response:', res)
 
     request_body = {'settings': {'number_of_shards': 1, 'number_of_replicas': 0}}
-    print('Creating index', index_name)
+    print('\nCreating index', index_name)
     res = es.indices.create(index=index_name, body=request_body)
     print('Response:', res)
 
@@ -51,19 +121,36 @@ def create_index(data, index_name='addressbase', type_name='hybrid'):
         bulk_data.append(op_dict)
         bulk_data.append(data_dict)
 
-    print('Populating data...')
+    print('\nPopulating data...')
     res = es.bulk(index=index_name, body=bulk_data, refresh=True)
 
 
 def test(index_name='addressbase'):
-    print('Testing index...')
+    """
+    Run a simple match_all query with size = 1 to check that something has been populated.
+    to the given index.
+
+    :param index_name: name of the index to query
+    :type index_name: str
+
+    :return: None
+    """
+    print('\nTesting index...')
     es = Elasticsearch(hosts=[{'host': 'localhost', 'port': 9200}])
     res = es.search(index=index_name, size=1, body={'query': {'match_all': {}}})
     print('Response:', res)
 
 
 def test2(index_name='addressbase'):
-    print('Testing index...')
+    """
+    Run a simple boolean query to test that the index has been populated.
+
+    :param index_name: name of the index to query
+    :type index_name: str
+
+    :return: None
+    """
+    print('\nTesting index...')
     es = Elasticsearch(hosts=[{'host': 'localhost', 'port': 9200}])
     res = es.search(index=index_name, body={'query': {'bool': {'should': [{'match': {'POSTCODE': 'CF48 2NQ'}},
                                                                           {'match': {'BUILDING_NAME': 'PENLAN'}}],
@@ -75,6 +162,6 @@ def test2(index_name='addressbase'):
 
 if __name__ == '__main__':
     data = load_data()
-    create_index(data)
+    create_and_populate_index(data)
     test()
     test2()
